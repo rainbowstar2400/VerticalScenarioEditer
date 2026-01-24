d26960d4411390677460d38307e6723a5b4a01f1
優 <rainbowstar2400@gmail.com>
2026-01-24 16:20:45 +0900

    Fix

--- VerticalScenarioEditer/RoleDictionaryWindow.xaml.cs
+++ VerticalScenarioEditer/RoleDictionaryWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
 using VerticalScenarioEditer.Models;
 using VerticalScenarioEditer.ViewModels;
 
